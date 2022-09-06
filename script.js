bd449d17a986107a1c5a30b74dd4f876398e8976
ysblfranco <112154548+ysblfranco@users.noreply.github.com>
2022-09-06 15:06:52 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -82,3 +82,20 @@ modalCloses.forEach((modalClose) => {
         })
     })
 })
+
+
+
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc(){
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+});
+
+
